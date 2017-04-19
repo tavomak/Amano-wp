a529a6ee03aa1c9ccbe27dc3feea63c7fb3445b5
Gustavo Rivero <tavomak@gmail.com>
2017-04-18 22:19:24 -0400

    Loading

--- includes/header.php
+++ includes/header.php
@@ -22,15 +22,7 @@
         <div class="wrapper-loader">
     <!--       <div class="preloader sk-cube"></div>-->
             <div class="sk-cube-grid">
-                <div class="sk-cube sk-cube1"></div>
-                <div class="sk-cube sk-cube2"></div>
-                <div class="sk-cube sk-cube3"></div>
-                <div class="sk-cube sk-cube4"></div>
-                <div class="sk-cube sk-cube5"></div>
-                <div class="sk-cube sk-cube6"></div>
-                <div class="sk-cube sk-cube7"></div>
-                <div class="sk-cube sk-cube8"></div>
-                <div class="sk-cube sk-cube9"></div>
+                <div class="sk-cube sk-cube"></div>
                 <br>
                 <p class="text-center">Cargando...</p>
             </div>
