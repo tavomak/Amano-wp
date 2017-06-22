dfdfb5647a9a0d31f7ce433804c89f59f39626c2
Gustavo Rivero <tavomak@gmail.com>
2017-06-22 19:08:02 -0400

    Login

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,39 +1,58 @@
-<nav class="container woo_user_nav">
+<!--Navegacion de usuarios-->
+<nav class="container">
+  <div class="row">
+    <ul class="woo_user_nav">
 
-  <!--<div class="col-sm-7 col-lg-9 woo-user-nav__item">
-    <?php// do_shortcode('[woocommerce_product_search]'); ?>
-  </div>-->
-
-  <div class="col-sm-6 woo_user_nav__item">
-    <ul class="woo_user_nav__ul">
-      <li class="woo_user_nav__li">
+      <li class="woo_user_nav__item">
         <div class="current_user">
-
           <?php
+
           global $current_user;
           get_currentuserinfo();
-          if (is_user_logged_in()){
-              //echo get_avatar( $current_user->ID, 64 );
-               echo '<a href="'.get_permalink( get_option('woocommerce_myaccount_page_id') ).'" class="current_user__link"> Hola ' . $current_user->display_name . '</a>';
-          }else{
-              echo'<a href="'.get_permalink( get_option('woocommerce_myaccount_page_id') ).'" class="current_user__link">Login</a>';
-          }
-          ?>
 
+          if (is_user_logged_in()){
+            echo '
+              <div class="dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="loginMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                  Hola ' . $current_user->display_name . '
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="loginMenu">
+                  <li><a href="'.get_permalink( get_option('woocommerce_myaccount_page_id') ).'" class="woo-login-popup-sc-open">Tu cuenta</a></li>
+                  <li><a href="'.wp_logout_url( home_url('/') /*get_permalink(woocommerce_get_page_id( 'myaccount' ) )*/ ).'" class="current_user__link">Salir</a></li>
+                </ul>
+              </div>
+            ';
+            //echo '<a href="'.get_permalink( get_option('woocommerce_myaccount_page_id') ).'" class="current_user__link"> Hola ' . $current_user->display_name . '</a>';
+          }else{
+            echo '
+              <div class="dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="loginMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                  Login
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="loginMenu">
+                  <li><a href="#" class="woo-login-popup-sc-open">Ingresar</a></li>
+                  <li><a href="#woo-login-popup-sc-register" class="woo-login-popup-sc-open">Crear cuenta</a></li>
+                </ul>
+              </div>
+            ';
+            //echo'<a href="'.get_permalink( get_option('woocommerce_myaccount_page_id') ).'" class="current_user__link">Login</a>';
+          }
+
+          ?>
         </div>
       </li>
 
-
       <?php
+
       global $woocommerce;
       $cart_url = $woocommerce->cart->get_cart_url();
 
       if ( class_exists( 'WooCommerce' ) ) {
-        // Si woocommerce exsiste
         $cartUrl = wc_get_cart_url();
         $cart_contents_count = WC()->cart->get_cart_contents_count();
         if ($cart_contents_count !== 0) {
-          // Si articulos en el carrito muestra el numero y el monto
           echo '<li class="woo_user_nav__li"><div class="cart_count"><a href="'.$cartUrl.'" class="cart_count__link"><span class="number_count">'.sprintf ( _n( '%d', '%d', $cart_contents_count ), $cart_contents_count ).'</span>'. WC()->cart->get_cart_total() .' </a></div></li>';
         }/*else{
             echo '<a href="'.$cart_url.'" class="cart_count__link">carrito</a>';
@@ -42,12 +61,28 @@
 
       ?>
 
-
-
-      <li class="woo_user_nav__li">
-        <a type="button" data-toggle="modal" data-target="#modal_reg_pago" class="register__link">Registra tu pago</a></li>
+      <li class="fl">
+        <a type="button" data-toggle="modal" data-target="#modal_reg_pago" class="btn btn-default">Registra tu pago</a>
       </li>
 
     </ul>
   </div>
 </nav>
+
+<!--Ventana modal para registro de deposito-->
+<div class="modal_reg_pago">
+  <div id="modal_reg_pago" tabindex="-1" role="dialog" class="modal fade">
+    <div role="document" class="modal-dialog modal-lg">
+      <div id="modalbox" class="modal-content">
+        <div class="modal-header">
+            <button type="button" data-dismiss="modal" aria-label="Cerrar" class="close"><span aria-hidden="true">&times;</span></button>
+            <h3 class="modal-tittle text-center">Registra tu pago</h3> </div>
+        <div class="modal-body">
+            <div>
+                <?php echo do_shortcode( '[contact-form-7 id="802" title="Registrar Pago"]' ); ?>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- includes/header.php
+++ includes/header.php
@@ -1,90 +1,75 @@
 <!DOCTYPE html>
 <?php tha_html_before(); ?>
 <html class="no-js">
-    <head>
-        <?php tha_head_top(); ?>
-        <title><?php wp_title('|', true, 'right'); //bloginfo('name'); ?></title>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php tha_head_bottom(); ?>
-        <?php wp_head(); ?>
-    </head>
 
-    <body <?php body_class(); ?>>
+  <head>
+    <?php tha_head_top(); ?>
+    <title><?php wp_title('|', true, 'right'); //bloginfo('name'); ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php tha_head_bottom(); ?>
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
     <?php tha_body_top(); ?>
 
-    <!--[if lt IE 8]>
-    <div class="alert alert-warning">
-        You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
+  <!--[if lt IE 8]><div class="alert alert-warning">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</div><![endif]-->
+
+    <!--Precargador-->
+    <div class="cd-loader">
+        <div class="cd-loader__grid">
+            <div class="cd-loader__item"></div>
+            <br>
+            <p class="text-center">Cargando...</p>
+        </div>
     </div>
-    <![endif]-->
 
-        <div class="cd-loader">
-    <!--       <div class="preloader sk-cube"></div>-->
-            <div class="cd-loader__grid">
-                <div class="cd-loader__item"></div>
-                <br>
-                <p class="text-center">Cargando...</p>
-            </div>
+    <!--Insercion de menu de usuario-->
+    <?php
+    if (is_woocommerce()){
+      echo get_template_part('includes/breadcrumb');
+    }
+    ?>
+
+    <!--Logotipo-->
+    <div class="logo-principal text-center">
+      <h1 class="text-center">
+        <a href="<?php echo home_url('/'); ?>">
+          <img src="<?php bloginfo('template_directory'); ?>/assets/img/CAMLogo.png" alt="Calzados a mano">
+        </a>
+      </h1>
+    </div>
+
+    <!--Navegacion principal-->
+    <nav class="navbar navbar-default navbar-static-top navbar-upper nav-principal-cam">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-upper">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+            <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> */ ?>
         </div>
-
-        <?php
-          if (is_woocommerce()){
-            echo get_template_part('includes/breadcrumb');
-          }
-        ?>
-
-        <div class="modal_reg_pago">
-          <div id="modal_reg_pago" tabindex="-1" role="dialog" class="modal fade">
-              <div role="document" class="modal-dialog modal-lg">
-                  <div id="modalbox" class="modal-content">
-                      <div class="modal-header">
-                          <button type="button" data-dismiss="modal" aria-label="Cerrar" class="close"><span aria-hidden="true">&times;</span></button>
-                          <h3 class="modal-tittle text-center">Registra tu pago</h3> </div>
-                      <div class="modal-body">
-                          <div>
-                              <?php echo do_shortcode( '[contact-form-7 id="802" title="Registrar Pago"]' ); ?>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
+        <div class="collapse navbar-collapse" id="navbar-upper">
+          <?php
+          wp_nav_menu( array(
+            'menu'              => 'primary',
+            'theme_location'    => 'primary',
+            'depth'             => 3,
+            //'container_class' => 'nav-menu-primary-container',
+            'menu_class'        => 'nav navbar-nav text-center',
+            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+            'walker'            => new wp_bootstrap_navwalker())
+          );
+          ?>
+          <?php //get_template_part('includes/navbar-search'); ?>
         </div>
+      </div>
+    </nav>
 
-        <div class="logo-principal text-center">
-           <h1 class="text-center">
-               <a href="<?php echo home_url('/'); ?>">
-                   <img src="<?php bloginfo('template_directory'); ?>/assets/img/CAMLogo.png" alt="Calzados a mano">
-               </a>
-           </h1>
-       </div>
-
-        <nav class="navbar navbar-default navbar-static-top navbar-upper nav-principal-cam">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-upper"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                    <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>">
-                        <?php bloginfo('name'); ?>
-                            </a> */ ?> </div>
-                <div class="collapse navbar-collapse" id="navbar-upper">
-                    <?php
-                                wp_nav_menu( array(
-                                    'menu'              => 'primary',
-                                    'theme_location'    => 'primary',
-                                    'depth'             => 3,
-                                    //'container_class' => 'nav-menu-primary-container',
-                                    'menu_class'        => 'nav navbar-nav text-center',
-                                    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                    'walker'            => new wp_bootstrap_navwalker())
-                                );
-                            ?>
-                        <?php //get_template_part('includes/navbar-search'); ?>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-            <!-- /.container -->
-        </nav>
-        <?php tha_header_before(); ?>
-            <!--<div class="wrapper">-->
-<hr class="hidden-xs">
+    <?php tha_header_before(); ?>
+    <!--<div class="wrapper">-->
