bf5f69e7e28c3b04822147fb310aaf48cb8b43a2
Gustavo Rivero <tavomak@gmail.com>
2017-07-01 14:06:29 -0400

    Ajustes menores HomePage y contacts.scss

--- page.php
+++ page.php
@@ -1,27 +1,28 @@
 <?php get_template_part('includes/header'); ?>
 
-<?php if ( is_front_page() ) : ?>
+  <?php if ( is_front_page() ) : ?>
 
-<section class="welcome">
+  <section class="welcome">
 
-  <div class="welcome-content container">
-    <div class="welcome-txt col-sm-4 col-lg-2 col-lg-offset-1">
-      <h2>Bienvenidos</h2>
-      <p>Somos una marca venezolana que produce de la mano de los mejores artesanos productos de la mejor calidad.</p>
+    <div class="welcome-content container">
+      <div class="welcome-txt col-sm-4 col-lg-2 col-lg-offset-1">
+        <h2>Bienvenidos</h2>
+        <p>Somos una marca venezolana que produce de la mano de los mejores artesanos productos de la mejor calidad.</p>
+      </div>
     </div>
+
+    <?php if ( wp_is_mobile() ) :?>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/img/welcome-mobile.jpg" alt="Calzados a mano" class="img-responsive welcome-img">
+    <?php else: ?>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/img/welcome.jpg" alt="Calzados a mano" class="img-responsive welcome-img">
+    <?php endif; ?>
+
+  </section>
+
+  <div class="text-center">
+    <p>Siempre encontrarás algo especial en</p>
+    <h2 class="welcome-subtitle">CALZADOS A´ MANO</h2>
   </div>
-  <?php if ( wp_is_mobile() ) :?>
-  <img src="<?php bloginfo('template_directory'); ?>/assets/img/welcome-mobile.jpg" alt="Calzados a mano" class="img-responsive welcome-img">
-  <?php else: ?>
-  <img src="<?php bloginfo('template_directory'); ?>/assets/img/welcome.jpg" alt="Calzados a mano" class="img-responsive welcome-img">
-  <?php endif; ?>
-
-</section>
-
-<div class="text-center">
-  <p>Siempre encontrarás algo especial en</p>
-  <h2 class="welcome-subtitle">CALZADOS A´ MANO</h2>
-</div>
 
 <?php endif; ?>
 
@@ -39,6 +40,7 @@
 <section class="adv-subibaja">
   <div class="container">
     <div class="row">
+
       <div class="col-sm-4 text-center">
         <div class="adv-subibaja-txt">
           <h3 class="adv-subibaja-title">Conoce nuestra marca</h3>
@@ -46,8 +48,11 @@
         </div>
         <a href="" class="cam-button">Ir a la tienda</a>
       </div>
+
       <div class="col-sm-7 col-sm-offset-1 text-center adv-subibaja-img">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/img/welcome-subibaja.jpg" alt=" zapatilla Subibaja"></div>
+        <img src="<?php bloginfo('template_directory'); ?>/assets/img/welcome-subibaja.jpg" alt=" zapatilla Subibaja" class="img-responsive">
+      </div>
+    
     </div>
   </div>
 </section>
