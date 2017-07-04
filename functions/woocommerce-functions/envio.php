ab29adb2327e7623162a4c343486353f5c2d1a5c
Gustavo Rivero <tavomak@gmail.com>
2017-07-04 19:13:12 -0400

    Ajustes estilos checkout, desactivado codigo de custom shipping

--- functions/woocommerce-functions/envio.php
+++ functions/woocommerce-functions/envio.php
@@ -1,9 +1,6 @@
 <?php
 
-//require_once locate_template('/functions/woocommerce-functions/envio.php');
-/*
-Cambiar Texto de opcion de envio
-*/
+//Cambiar Texto de opcion de envio
 function wc_shipping_field_strings( $translated_text, $text, $domain ) {
     switch ( $translated_text ) {
         case '¿Enviar a una dirección diferente?' :
@@ -14,9 +11,7 @@ function wc_shipping_field_strings( $translated_text, $text, $domain ) {
 }
 add_filter( 'gettext', 'wc_shipping_field_strings', 20, 3 );
 
-/*
-Agregar Estados de Venezuela
-*/
+//Agregar Estados de Venezuela
 function custom_woocommerce_states( $states ) {
 
   $states['VE'] = array(
@@ -49,110 +44,6 @@ function custom_woocommerce_states( $states ) {
 add_filter( 'woocommerce_states', 'custom_woocommerce_states' );
 
 /*
-Agregar Estados de Venezuela
-*/
-function custom_woocommerce_cities( $cities ) {
-    global $cities;
-
-  $cities['VE'] = array(
-    'AM' => array(
-		'AM 1',
-		'AM 2'
-	),
-    'AN'  => array(
-		'AN 1',
-		'AN 2'
-	),
-    'AP'  => array(
-		'AP 1',
-		'AP 2'
-	),
-    'AR'  => array(
-		'AR 1',
-		'AR 2'
-	),
-    'BA'  => array(
-		'BA 1',
-		'BA 2'
-	),
-    'CA'  => array(
-		'CA 1',
-		'CA 2'
-	),
-    'CO'  => array(
-		'CO 1',
-		'CO 2'
-	),
-    'DA' =>array(
-		'DA 1',
-		'DA 2'
-	),
-    'DC' => array(
-		'DC 1',
-		'DC 2'
-	),
-    'FA' => array(
-		'FA 1',
-		'FA 2'
-	),
-    'GU'  => array(
-		'GU 1',
-		'GU 2'
-	),
-    'LA'  => array(
-		'LA 1',
-		'LA 2'
-	),
-    'ME'  => array(
-		'ME 1',
-		'ME 2'
-	),
-    'MI'  => array(
-		'MI 1',
-		'MI 2'
-	),
-    'MO'  => array(
-		'MO 1',
-		'MO 2'
-	),
-    'NE'  => array(
-		'NE 1',
-		'NE 2'
-	),
-    'PO'  => array(
-		'PO 1',
-		'PO 2'
-	),
-    'SU'  => array(
-		'SU 1',
-		'SU 2'
-	),
-    'TA'  => array(
-		'TA 1',
-		'TA 2'
-	),
-    'TR'  => array(
-		'TR 1',
-		'TR 2'
-	),
-    'VA'  => array(
-		'VA 1',
-		'VA 2'
-	),
-    'YA'  => array(
-		'YA 1',
-		'YA 2'
-	),
-    'ZU'  => array(
-		'ZU 1',
-		'ZU 2'
-	)
-  );
-  return $cities;
-}
-add_filter( 'woocommerce_cities', 'custom_woocommerce_cities' );
-
-/*
 Reordenar Campos en el checkout
  */
 function reorder_checkout( $fields ) {
@@ -160,13 +51,14 @@ function reorder_checkout( $fields ) {
     unset($fields['billing']['billing_company']);
     unset($fields['billing']['billing_postcode']);
     unset($fields['billing']['billing_country']);
+    unset($fields['billing']['billing_address_2']);
     unset($fields['shipping']['shipping_company']);
     unset($fields['shipping']['shipping_address_1']);
     unset($fields['shipping']['shipping_address_2']);
     unset($fields['shipping']['shipping_country']);
     unset($fields['shipping']['shipping_postcode']);
 
-    /*$cities = array( 'opt1' => 'Barquisimeto', 'opt2' => 'Maracaibo', 'opt3' => 'Valencia', 'opt4' => 'Puerto Ordas', 'opt5' => 'Puerto Ayacucho', );*/
+    $fields['billing']['billing_address_1']['type'] = 'textarea';
 
     //Cambiar titulo de estado
     $fields['shipping']['shipping_state']['placeholder'] = 'Estado';
@@ -184,9 +76,9 @@ add_filter( 'woocommerce_checkout_fields' , 'reorder_checkout' );
 
 /*
 Crear campo para Ciudad
-*/
+
 function custom_city_field( $fields ) {
-    /*$estado = WC()->customer->get_shipping_state();
+    $estado = WC()->customer->get_shipping_state();
     switch ($estado) {
         case (AM):
             $opciones = array(
@@ -236,7 +128,7 @@ function custom_city_field( $fields ) {
                   'option_4' => 'agencia AM4'
               );
                 break;
-        }*/
+        }
 
     $fields['shipping']['zoom_pickup'] = array(
         'type'     => 'select',
@@ -249,7 +141,7 @@ function custom_city_field( $fields ) {
 
     return $fields;
 }
-add_filter( 'woocommerce_checkout_fields' , 'custom_city_field');
+add_filter( 'woocommerce_checkout_fields' , 'custom_city_field');*/
 
 /**
 Update the order meta with store location pickup value
@@ -269,108 +161,5 @@ function my_woocommerce_email_order_meta_keys( $keys ) {
     return $keys;
 }
 add_filter('woocommerce_email_order_meta_keys', 'my_woocommerce_email_order_meta_keys');
-
-/*
-Replace XX with the country code. Instead of YYY, ZZZ use actual  state codes.
-*/
-function my_cities( $cities ) {
-
-	$cities['VE'] = array(
-        'AM' => array(
-            'AM 1',
-            'AM 2'
-        ),
-        'AN'  => array(
-            'AN 1',
-            'AN 2'
-        ),
-        'AP'  => array(
-            'AP 1',
-            'AP 2'
-        ),
-        'AR'  => array(
-            'AR 1',
-            'AR 2'
-        ),
-        'BA'  => array(
-            'BA 1',
-            'BA 2'
-        ),
-        'CA'  => array(
-            'CA 1',
-            'CA 2'
-        ),
-        'CO'  => array(
-            'CO 1',
-            'CO 2'
-        ),
-        'DA' =>array(
-            'DA 1',
-            'DA 2'
-        ),
-        'DC' => array(
-            'DC 1',
-            'DC 2'
-        ),
-        'FA' => array(
-            'FA 1',
-            'FA 2'
-        ),
-        'GU'  => array(
-            'GU 1',
-            'GU 2'
-        ),
-        'LA'  => array(
-            'LA 1',
-            'LA 2'
-        ),
-        'ME'  => array(
-            'ME 1',
-            'ME 2'
-        ),
-        'MI'  => array(
-            'MI 1',
-            'MI 2'
-        ),
-        'MO'  => array(
-            'MO 1',
-            'MO 2'
-        ),
-        'NE'  => array(
-            'NE 1',
-            'NE 2'
-        ),
-        'PO'  => array(
-            'PO 1',
-            'PO 2'
-        ),
-        'SU'  => array(
-            'SU 1',
-            'SU 2'
-        ),
-        'TA'  => array(
-            'TA 1',
-            'TA 2'
-        ),
-        'TR'  => array(
-            'TR 1',
-            'TR 2'
-        ),
-        'VA'  => array(
-            'VA 1',
-            'VA 2'
-        ),
-        'YA'  => array(
-            'YA 1',
-            'YA 2'
-        ),
-        'ZU'  => array(
-            'ZU 1',
-            'ZU 2'
-        )
-	);
-	return $cities;
-}
-add_filter( 'wc_city_select_cities', 'my_cities' );
 
 ?>
