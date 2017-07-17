3f72832f2e81088bf0050e544257f50889fb6944
Gustavo Rivero <tavomak@gmail.com>
2017-07-17 12:07:07 -0400

    Envios Zoom Terminados! XD

--- assets/js/pickup-locations.js
+++ assets/js/pickup-locations.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 
     var currentState = $('#shipping_state').val();
+    var currentCity = $('#shipping_city').val();
+    var currentAgency = $('#shipping_state').val();
     console.log(currentState);
     console.log(ajax_object[currentState]);
 
-    function ajax_object_iteration() {
+    function ajax_object_city_iteration() {
         $.each(ajax_object[currentState], function (ciudad, agencia) {
             $.each(agencia, function (nombre, direccion) {});
             $('#shipping_city').append($('<option>', {
@@ -14,16 +16,26 @@ $(document).ready(function () {
         });
     }
 
-    $('#shipping_state option:first-child').val('Elige');
+    function ajax_object_agency_iteration() {
+        $.each(ajax_object[currentState][currentCity], function (nombre, direccion) {
+            $('#shipping_agency').append($('<option>', {
+                value: nombre,
+                title: direccion
+            }).text(nombre));
+        });
+    }
 
-    if (currentState == 'Elige') {
-        $('#shipping_city_field').hide();
-        $('#shipping_agency_field').hide();
-    } else {
-        ajax_object_iteration();
+    $('#shipping_state option:first-child').val('Elige');
+    $('#shipping_city_field').hide();
+    $('#shipping_agency_field').hide();
+
+    if (currentState !== 'Elige' || currentState !== null || currentState !== '') {
+        ajax_object_city_iteration();
+        $('#shipping_city_field').show();
     }
 
     $('#shipping_state').on('change', function () {
+        $('#shipping_city_field').hide('fast');
         currentState = $('#shipping_state').val();
         console.log(currentState);
         $('#shipping_city option').each(function () {
@@ -33,20 +45,31 @@ $(document).ready(function () {
             value: 'Seleccionar Ciudad',
             title: 'Seleccionar Ciudad'
         }).text('Seleccionar Ciudad'));
-        $.ajax({
-            cache: false,
-            type: "POST",
-            url: ajax_object.ajax_url,
-            beforeSend: function () {
-                $('.woocommerce-shipping-fields').css('background', 'yellow');
-            },
-            complete: function () {
-                $('.woocommerce-shipping-fields').css('background', 'none');
-            },
-            data: {
-                action: 'custom_city_field',
-            },
-            success: ajax_object_iteration()
+
+        ajax_object_city_iteration();
+        $('#shipping_city_field').show('slow');
+        $('#shipping_agency option').each(function () {
+            $(this).remove();
         });
+        $('#shipping_agency_field').hide();
+
     });
+
+    $('#shipping_city').on('change', function () {
+        $('#shipping_agency_field').hide('fast');
+        currentCity = $('#shipping_city').val();
+        console.log(currentCity);
+        $('#shipping_agency option').each(function () {
+            $(this).remove();
+        });
+        $('#shipping_agency').append($('<option>', {
+            value: 'Seleccionar Agencia',
+            title: 'Seleccionar Agencia'
+        }).text('Seleccionar Agencia'));
+
+        ajax_object_agency_iteration();
+        $('#shipping_agency_field').show('slow');
+
+    });
+
 });
