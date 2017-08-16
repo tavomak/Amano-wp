f412572037718295da2137bf895bf78db1cee7b5
Gustavo Rivero <tavomak@gmail.com>
2017-08-16 16:31:16 -0400

    gulp beautify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp')
-  , sass = require('gulp-sass'), //Copila SCSS
+var gulp = require('gulp'),
+  sass = require('gulp-sass'), //Copila SCSS
   autoprefixer = require('gulp-autoprefixer'), //Autoprefixer
   browserSync = require('browser-sync').create(), //Recarga el navegador
   concat = require('gulp-concat'), //Concatena archivos js (hay ubo para css)
@@ -33,7 +33,9 @@ gulp.task('minify-css', function () {
     .pipe(rename({
       suffix: '-dist'
     }))
-    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
     .pipe(cleanCSS())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./assets/css/'))
@@ -42,16 +44,18 @@ gulp.task('minify-css', function () {
       message: 'Css Procesado',
       onLast: true
     }))
-    .pipe(browserSync.stream({match: '**/*.css'}));
+    .pipe(browserSync.stream({
+      match: '**/*.css'
+    }));
 });
 
 gulp.task('minify-js', function () {
   return gulp.src('./assets/js/main.js')
     .pipe(uglify()
-    .on("error", notify.onError({
-      sound: true,
-      title: 'Error en JS'
-    })))
+      .on("error", notify.onError({
+        sound: true,
+        title: 'Error en JS'
+      })))
     .pipe(rename({
       suffix: '-dist'
     }))
@@ -60,22 +64,22 @@ gulp.task('minify-js', function () {
       sound: false,
       message: 'Js Procesados',
       onLast: true
-  }));
+    }));
 });
 
 gulp.task('watch', function () {
   browserSync.init({
-    files: ['./*.php', './*.css', './*.js', './*.scss', './*.jpg', './*.png']
-    , proxy: "http://localhost/amano/"
-    , notify: true
+    files: ['./*.php', './*.css', './*.js', './*.scss', './*.jpg', './*.png'],
+    proxy: "http://localhost/amano/",
+    notify: true
   });
   gulp.watch('./assets/scss/**/*.scss', ['sass']);
   gulp.watch('./assets/css/**/*.css', ['minify-css']);
   gulp.watch('./**/*.php').on('change', function () {
     notify({
-      sound: false
-      , message: 'PHP Procesado'
-      , onLast: true
+      sound: false,
+      message: 'PHP Procesado',
+      onLast: true
     }).write('');
     browserSync.reload();
   });
